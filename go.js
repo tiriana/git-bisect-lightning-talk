6e884bdc9eb7dcae000434622e0717b814b4e115
Radomir Wojtera <radomir.wojtera@gmail.com>
2016-02-29 15:19:34 +0100

    Trzeci

--- go.js
+++ go.js
@@ -1,3 +1,8 @@
 var showRainbow = require('./lib/showRainbow');
 var text = require('./lib/text.json');
+
+function fail() {
+    throw new Error('oh dear, oh dear...');
+}
+
 showRainbow(text, 10, 1000);
